891e2f3e6e9c3a36688470eb2b5be06f80b87514
thanawutmalirum29-creator <thanawutmalirum29@gmail.com>
2026-06-16 10:38:52 +0700

    Add files via upload

---
+++ surver/locationData.js
@@ -0,0 +1,17 @@
+const locationList = [
+  { id: "river",      name: "ลำธาร" },
+  { id: "sea",        name: "ทะเล" },
+  { id: "lava",       name: "ลาวา" },
+  { id: "candyland",  name: "แดนขนมหวาน" },
+  { id: "deepsea",    name: "ทะเลลึก" },
+  { id: "ancientsea", name: "ทะเลโบราณ" },
+  { id: "bonerealm",  name: "แดนกระดูก" },
+  { id: "skyrealm",   name: "ดินแดนเหนือเมฆ" },
+  { id: "toxicmarsh", name: "บึงพิษ" },
+  { id: "frostlake",  name: "ทะเลสาบน้ำแข็ง" },
+  { id: "mooncrater", name: "หลุมอุกกาบาตบนดวงจันทร์" },
+  { id: "clocktower", name: "หอนาฬิกาโบราณ" },
+  { id: "dreamgrove", name: "ป่าความฝัน" }
+];
+
+module.exports = { locationList };
